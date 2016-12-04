eb46eb91d691ac79dd4668f6a90f7c1ed6d7a5cb
mbhatt330 <mbhatt330@gmail.com>
2016-12-04 19:22:06 +0530

    Create code.php

---
+++ code.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<script language="javascript">
+$(function(){
+var m=document.getElementById('txt').value;
+var n=(this).value;
+this.parent().siblings().value=n;
+.on('change','.deleteme',function(){
+
+
+</script>
+</body>
+
+</html>
